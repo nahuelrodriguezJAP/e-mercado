208a0e682b5ac68da87bd7ba3c1ca1a29dc32fc7
nahuelrodriguezJAP <Soccernahu@gmail.com>
2021-09-15 22:53:50 -0300

    mostrar comentarios y tama;o fotos

--- js/mostrarProducto.js
+++ js/mostrarProducto.js
@@ -1,34 +1,27 @@
 var detalleProducto ="";
-var comentarios="";
+var comentarios=[];
 
 function showMostrar(detalleProducto) {
     let info = "";
-    let fotos = ` {
-        <img class="img" src="img/${localStorage.getItem('auto')}/prod1.jpg>",
-        <img class="img" src="img/${localStorage.getItem('auto')}/prod2.jpg>",  
-        <img class="img" src="img/${localStorage.getItem('auto')}/prod3.jpg>",
-        <img class="img" src="img/${localStorage.getItem('auto')}/prod4.jpg>"
-        <img class="img" src="img/${localStorage.getItem('auto')}/prod5.jpg>"    
-        }`
     info += `
     <br><br> 
     <br><br><h2> <strong>${detalleProducto.name} </strong></h2><br>
-    <div id="fotos"><img class="img" src="${detalleProducto.images[0]}"><img class="img" src="${detalleProducto.images[1]}"><img class="img" src="${detalleProducto.images[2]}"><img class="img" src="${detalleProducto.images[3]}"><img class="img" src="${detalleProducto.images[4]}"></p> </div>
+    <div id="fotos"><img class="img" width="720" src="${detalleProducto.images[00]}"><img class="img"  width="720" src="${detalleProducto.images[1]}"><img class="img"  width="720" src="${detalleProducto.images[2]}"><img class="img"  width="720" src="${detalleProducto.images[3]}"><img class="img"  width="720" src="${detalleProducto.images[4]}"></p> </div>
     <p> ${detalleProducto.description} </p><br><p>${detalleProducto.category}</p><p>Precio: ${detalleProducto.cost} ${detalleProducto.currency}  Productos Vendidos${detalleProducto.soldCount}</p>
     `
-    
-    console.log(detalleProducto)
     document.getElementById("contenido").innerHTML = info;
 }
 
-function showComents(comments){
-    let comentarios = "<br><h2> Comentarios de Clientes: </h2><br>";
-    comentarios +=`
+function showComents(){
+    let coments = "<br><h3> Comentarios de Clientes: </h3><br>";
+    for (let i = 0; i < comentarios.length; i++) {
+        let comments = comentarios[i];
+    coments+=`
     <p> ${comments.description} dice: ${comments.user} </p>
     <p> Día: ${comments.dateTime} horas.</p>
     `
-
-    document.getElementById("comentarios").innerHTML+=comentarios;
+    }
+    document.getElementById("comentarios").innerHTML+=coments;
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -41,8 +34,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
         
     })
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(result){
-        if(result.status=== 'ok'){ 
-            let comentarios=result.data;
+        if(result.status=== 'ok'){
+            comentarios=result.data;
             showComents(comentarios);
 
 
